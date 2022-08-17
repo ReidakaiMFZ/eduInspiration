54fbe457ed18397f8a0a032ea3801c2f18bec8b4
cauarojas <cauarojas@hotmail.com>
2022-08-17 20:26:28 -0300

    all ready to begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-    return <h1 className='font-bold'>Hello World!</h1>;
+    return <h1 className='font-bold text-4xl'>Hello World!</h1>;
 }
 
 export default App;
