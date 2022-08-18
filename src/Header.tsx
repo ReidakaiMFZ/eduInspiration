dd0e0f271b50539926177536fcbce1a225ffc3c1
cauarojas <cauarojas@hotmail.com>
2022-08-18 19:21:44 -0300

    some of the header is ready...

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,10 @@
+import hamburger from './assets/hamburger-menu.svg';
 export default function Header() {
     return (
-        <header>
+        <header className='w-full bg-slate-600  h-16 flex flex-row'>
+            <button>
+                <img src={hamburger} alt={'Menu'} className='w-16' />
+            </button>
             <h1>Header</h1>
         </header>
     );
