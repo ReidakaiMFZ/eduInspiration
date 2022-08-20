ecf8bbbb324e26d01c558d6984eaeaf16def30d5
cauarojas <cauarojas@hotmail.com>
2022-08-20 14:12:13 -0300

    added text to front page

--- src/FrontPage.tsx
+++ src/FrontPage.tsx
@@ -2,11 +2,30 @@ import { Link } from 'react-router-dom';
 
 export default function FrontPage() {
     return (
-        <div className='min-h-max min-w-max'>
-            <h1 className='font-bold text-4xl'>Pagina Inicial!</h1>
-            <Link to={'/about'} className='text-sm'>
-                Sobre Nós
-            </Link>
+        <div className='mt-20 min-h-max min-w-max flex flex-row'>
+            <div className='w-1/2 flex flex-col align-middle text-center'>
+                <span className='font-bold inline-block text-gpink text-4xl'>
+                    Prazer somos a
+                </span>
+                <span className='font-bold text-4xl inline-block'>
+                    "Nome do grupo"
+                </span>
+                <p className='mt-12 text-2xl'>
+                    Uma plataforma focada na resolução de problemas reais
+                </p>
+                <p className='mt-8 text-xl'>
+                    Aqui você pode solucionar problemas reais de empresas, e
+                    usar a ideia como tcc
+                </p>
+                <Link
+                    to={'/register'}
+                    className='mt-12 text-white text-2xl underline p-2 hover:text-3xl transition-all duration-500'>
+                    Cadastre-se
+                </Link>
+            </div>
+            <div className='w-1/2 px-36 '>
+                <img src='#' alt='Foto' className='w-full h-full bg-gpink' />
+            </div>
         </div>
     );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -13,12 +13,26 @@ function UnLoggedHeader() {
         <header className='w-full p-2 h-18 flex flex-row items-center justify-between'>
             <img src='#' alt='Logo' className='w-1/12 h-full' />
             <section className='w-1/6 text-lg flex gap-8 flex-row justify-between'>
-                <Link to={'/business'} className='h-8 rounded underline'>Empresas</Link>
-                <Link to={'/school'} className='h-8 rounded underline'>Escolas</Link>
-                <Link to={'/about'} className='h-8 rounded underline'>Sobre Nós</Link>
+                <Link
+                    to={'/business'}
+                    className='h-8 rounded underline hover:text-xl transition-all duration-500'>
+                    Empresas
+                </Link>
+                <Link
+                    to={'/school'}
+                    className='h-8 rounded underline hover:text-xl transition-all duration-500'>
+                    Escolas
+                </Link>
+                <Link
+                    to={'/about'}
+                    className='h-8 rounded underline hover:text-xl transition-all duration-500'>
+                    Sobre Nós
+                </Link>
             </section>
-            <section className='w-1/3 items-end text-lg flex gap-4 flex-row-reverse'>
-                <button className='h-8 w-1/12 rounded underline'>Login</button>
+            <section className='w-1/3 items-end text-lg flex gap-4 flex-row-reverse '>
+                <button className='h-8 w-1/12 rounded underline hover:text-xl transition-all duration-500'>
+                    Login
+                </button>
             </section>
         </header>
     );
