efc415861584ca1315f62c6c2bb98ab8a256dbdd
ReidakaiMFZ <richard_maldito2@hotmail.com>
2022-08-25 00:09:00 -0300

    FrontPageLogged added

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import { Link, Routes, Route } from 'react-router-dom';
 import AboutUs from './AboutUs';
 import Register from './Register';
 import Login from './Login';
+import ProjectView from './ProjectView';
+import FrontPageLogged from './FrontPageLogged';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebaseObjs';
 
 function App() {
+    const [user] = useAuthState(auth);
     return (
         <main className='bg-gblack w-full h-full'>
             <Header />
             <Routes>
-                <Route path='/' element={<FrontPage />} />
+                <Route path='/' element={user ? <FrontPageLogged /> : <FrontPage/>} />
+
                 <Route path='/about' element={<AboutUs />} />
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/projectview' element={<ProjectView />} />
             </Routes>
         </main>
     );
--- src/ProjectView.tsx
+++ src/ProjectView.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 export default function ProjectView() {
   return (
     <div className="flex flex-nowrap flex-row items-stretch">
-        <nav className="grid  h-screen w-1/2 justify-center">
+        <nav className="grid h-96 w-1/2 justify-center">
             <div className="flex justify-center mt-10">
                 <img src="https://static.poder360.com.br/2020/11/bolsonaro-twitter-meme-18-nov.gif" alt="problema" className=''/>
             </div>
@@ -14,12 +14,12 @@ export default function ProjectView() {
             </div>
         </nav>
 
-        <div className="min-h-full max-h-screen w-1/2 flex flex-col gap-y-2 mt-10">
+        <div className="h-full w-1/2 flex flex-col gap-y-2 mt-8">
             <h1 className="bold text-5xl">Problema</h1>
             <h6 className="mt-2 mb-5">curso/profissão</h6>
             <p className="break-all h-full">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat consequatur veniam porro eaque, cupiditate alias consequuntur? Dolorem laborum provident repudiandae, magni voluptatum cupiditate nisi iusto voluptas quasi eligendi molestiae repellendus.</p>
             <div className="flex flex-row-reverse mb-10 mr-10">
-                <button type="submit" className="bg-gpink w-64 h-16 rounded-lg">Casar com o projeto</button>
+                <button type="submit" className="bg-gpink w-64 h-16 mt-8 rounded-lg">Casar com o projeto</button>
             </div>
         </div>
     </div>
